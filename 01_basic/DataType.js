ad5ae02e150ddd60512cb63aea714e0362ca3bec
Hari06om <hariohmverma96@gmail.com>
2025-10-09 20:37:05 +0530

    dataTypes java

---
+++ 01_basic/DataType.js
@@ -0,0 +1,13 @@
+"use strict";
+// Data Types in JavaScript
+
+console.log("Data Types in JavaScript");
+
+console.log(typeof undefined);
+console.log(typeof null); // object
+console.log(typeof true);
+console.log(typeof 123);
+console.log(typeof 123n);
+console.log(typeof "string");
+console.log(typeof Symbol("id"));
+console.log(typeof {});
