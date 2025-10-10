4628fe532a991bbb4b56ce8f747250c5febaac12
Hari06om <hariohmverma96@gmail.com>
2025-10-10 18:06:37 +0530

    comparsion betwenn all  the thing every part of the thing

---
+++ 01_basic/comparsion.js
@@ -0,0 +1,25 @@
+//*************************************normal conversion /***************** */ */
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// ********************************strict conversion /************************************** */ */
+
+
+console.log(2=="2");
+console.log(2==="2");
+console.log(2!="2");
+console.log(2!=="2");
+
+//*******************************   null converesion **********************************/
+console.log( "// *********************************************//");
+
+console.log(null ==0);
+console.log(null >0);
+console.log(null >=0);
+
+
