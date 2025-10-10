9ff32af9e40b543f55775159d81176c0c46ceedb
Hari06om <hariohmverma96@gmail.com>
2025-10-10 17:38:49 +0530

    learn the conversionOperation

---
+++ 01_basic/conversion_data.js
@@ -0,0 +1,17 @@
+let score = "22ActiveXObject"
+
+console.log(typeof score) // number
+console.log(typeof(score)) // number4
+
+let value = Number(score) 
+console.log(typeof value) // number
+console.log( value) // number
+
+let inloggedIn = " hari"
+let booleanInloggedIn = Boolean(inloggedIn)
+console.log(booleanInloggedIn) // true
+
+let someNumber = 123
+let stringNumber = String(someNumber)
+console.log(stringNumber) // "123"
+console.log(typeof stringNumber) // string;
