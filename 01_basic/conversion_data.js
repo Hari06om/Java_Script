945fe3e231275ebb5f724f4447449c67bf74e7d3
Hari06om <hariohmverma96@gmail.com>
2025-10-10 17:55:46 +0530

    learn the conversionOperation

--- 01_basic/conversion_data.js
+++ 01_basic/conversion_data.js
@@ -1,17 +1,50 @@
-let score = "22ActiveXObject"
+// let score = "22ActiveXObject"
 
-console.log(typeof score) // number
-console.log(typeof(score)) // number4
+// console.log(typeof score) // string
+// console.log(typeof(score)) // string
+// let value = Number(score) 
+// console.log(typeof value) // number
+// console.log( value) // number
 
-let value = Number(score) 
-console.log(typeof value) // number
-console.log( value) // number
+// let inloggedIn = " hari"
+// let booleanInloggedIn = Boolean(inloggedIn)
+// console.log(booleanInloggedIn) // true
 
-let inloggedIn = " hari"
-let booleanInloggedIn = Boolean(inloggedIn)
-console.log(booleanInloggedIn) // true
+// let someNumber = 123
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) // "123"
+// console.log(typeof stringNumber) // string;
 
-let someNumber = 123
-let stringNumber = String(someNumber)
-console.log(stringNumber) // "123"
-console.log(typeof stringNumber) // string;
+
+//  *************************  Operations  **************************************
+
+let val = 3
+let negvalue = -val
+console.log(negvalue) // -3
+
+
+
+// console.log(2+2) 
+// console.log("2"+"2")
+// console.log(2-2) 
+// console.log(2*2) 
+// console.log(2/2) 
+// console.log(2%2) 
+
+
+let str = "Hello"
+let str2 = " Hari"
+
+let str3 = str + " " + str2
+// console.log(str3) // Hello Hari
+
+
+// console.log("1"+2) // 12
+// console.log(1+"2") // 12
+// console.log("1"+2+2) // 122
+// console.log(1+2+"2") // 32;
+
+
+
+console.log(+true);
+
