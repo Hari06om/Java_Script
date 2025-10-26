797d637098bcdd021403d4701e03718d9fc4f3f4
Hari06om <hariohmverma96@gmail.com>
2025-10-26 15:56:32 +0530

    learn about number in js

---
+++ 01_basic/number_math.js
@@ -0,0 +1,15 @@
+const score = 100
+console.log(score);
+
+const balance = new Number(120)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const othernumber = 23.1234
+console.log(othernumber.toPrecision);
+
+const hunderds = 3000
+
+console.log(hunderds.toLocaleString('en-In'));
