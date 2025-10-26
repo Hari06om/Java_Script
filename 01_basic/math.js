cd436bcc68363020340dd37eec9b21b624742bab
Hari06om <hariohmverma96@gmail.com>
2025-10-26 16:07:20 +0530

    learn about the math injs

---
+++ 01_basic/math.js
@@ -0,0 +1,17 @@
+// Prints the entire Math object which contains mathematical functions and constants
+console.log(Math);
+
+// Returns the absolute (positive) value of -4, which is 4
+console.log(Math.abs(-4));
+
+// Rounds 4.6 to nearest integer (5 in this case)
+// Numbers >= .5 round up, < .5 round down
+console.log(Math.round(4.6));
+
+// Ceiling function - always rounds UP to the next integer
+// 3.5 becomes 4
+console.log(Math.ceil(3.5));
+
+// Generates random number between 0 and 4
+// Math.random() gives number between 0-1, multiplying by 4 gives range 0-4
+console.log(Math.random()*4);
