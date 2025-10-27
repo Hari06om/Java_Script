3cce44072bb03430a2d1b32ae298580c0b027808
Hari06om <hariohmverma96@gmail.com>
2025-10-27 20:29:02 +0530

    array in java SCript

--- Arrays/array2.js
+++ Arrays/array2.js
@@ -2,4 +2,8 @@ const marvel = ["thor ","Ironman","spiderman"]
 const dchero = ["superman","flash","batman"]
 
 marvel.push(dchero);
+// console.log(marvel);
+
+
+marvel.concat(dchero)
 console.log(marvel);
