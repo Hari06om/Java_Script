ea2f62065992c5ebad526db2ed8217e54690e845
Hari06om <hariohmverma96@gmail.com>
2025-10-28 20:31:28 +0530

    learn the object in Java Script

--- Arrays/Object.js
+++ Arrays/Object.js
@@ -3,17 +3,28 @@
 // Object literals 
 
 // Object.create
+
+
+const mySym = Symbol("Key1")
+
+
 const jsUser = {
     name : "Hari om Verma",
+    "Full name": "Nikhil Verma",
+    [mySym] : "MyKey1",
     age: 18,
     location: "punjab",
     email : "google.com",
     isLoggedIn : false ,
     lastLoginDay:["Monday","Tuesday"]
 
+
 }
 
 console.log(jsUser.age);
 console.log(jsUser["email"]);
+console.log(jsUser["Full name"]);
+console.log(jsUser[mySym]);
+
 
 
