423248d61ac46822d8c8042b91aab890c00a943c
Hari06om <hariohmverma96@gmail.com>
2025-10-28 20:26:02 +0530

    learn the object in Java Script

---
+++ Arrays/Object.js
@@ -0,0 +1,19 @@
+// singletone
+
+// Object literals 
+
+// Object.create
+const jsUser = {
+    name : "Hari om Verma",
+    age: 18,
+    location: "punjab",
+    email : "google.com",
+    isLoggedIn : false ,
+    lastLoginDay:["Monday","Tuesday"]
+
+}
+
+console.log(jsUser.age);
+console.log(jsUser["email"]);
+
+
