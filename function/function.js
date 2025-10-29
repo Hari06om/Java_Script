5c06de80c64f89da7cfb292fb434c7870e7f73af
Hari06om <hariohmverma96@gmail.com>
2025-10-29 19:38:23 +0530

    funciton in java SCript

---
+++ function/function.js
@@ -0,0 +1,25 @@
+
+/**
+ * Logs the letters of the word "HARIOM" to the console, one letter at a time.
+ *
+ * This function does not take any parameters and does not return any value.
+ * It is primarily used for demonstration or educational purposes to show
+ * how to log multiple lines to the console.
+ */
+function say(){
+    console.log("H");
+    console.log("A");
+    console.log("R");
+    console.log("I");
+    console.log("O");
+    console.log("M");
+
+}
+say()
+
+
+function  addtwonumber(a,b){
+   console.log(a+b);
+   
+}
+addtwonumber(5,6)
