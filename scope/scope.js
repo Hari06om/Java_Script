569fd914c7073d3b734d7e13107c55f6802ce1d5
Hari06om <hariohmverma96@gmail.com>
2025-10-30 20:41:22 +0530

    learn about scope in js :

--- scope/scope.js
+++ scope/scope.js
@@ -1,12 +1,31 @@
-let a = 10 
-const b = 20 
-var c = 30 
+// Declare a variable 'a' with block scope using 'let' and assign it the value 10
+// let a = 10 
 
+// Declare a constant 'b' with block scope using 'const' and assign it the value 20
+// const b = 20 
 
- {
+// Declare a variable 'c' with function scope using 'var' and assign it the value 30
+// var c = 30 
 
- }
- 
-console.log(a);
-console.log(b);
+// Start a block scope with an 'if' statement that always evaluates to true
+if(true){
+    // Declare a variable 'a' with block scope using 'let' and assign it the value 10
+    let a = 10 
+    
+    // Declare a constant 'b' with block scope using 'const' and assign it the value 20
+    const b = 20 
+    
+    // Declare a variable 'c' with function scope using 'var' and assign it the value 30
+    var c = 30 
+}
+
+// Attempt to log the value of 'a' to the console, but it will be undefined outside the block scope
+// console.log(a);
+
+// Attempt to log the value of 'b' to the console, but it will throw a ReferenceError since 'b' is block scoped
+// console.log(b);
+
+// Log the value of 'c' to the console, which will output 30 since 'c' is function scoped and accessible here
 console.log(c);
+
+
