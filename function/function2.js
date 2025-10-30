4b79d54c264e8bf9a780c4873530b3ad44174084
Hari06om <hariohmverma96@gmail.com>
2025-10-30 20:24:20 +0530

    learn how cart work

--- function/function2.js
+++ function/function2.js
@@ -1,5 +1,10 @@
-function calculateCartPrice(...num1){
+// function calculateCartPrice(...num1){
+//     return num1 
+// }
+
+// console.log(calculateCartPrice(200 ,300,500));
+function calculateCartPrice(var1,var2,...num1){
     return num1 
 }
 
-console.log(calculateCartPrice(200 ,300,500));
+console.log(calculateCartPrice(200 ,300,400,600,500));
