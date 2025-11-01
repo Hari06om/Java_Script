b9113f43aacf2ee329c940a3e00c1a0931457931
Hari06om <hariohmverma96@gmail.com>
2025-11-01 16:23:20 +0530

    invoake function

--- scope/life.js
+++ scope/life.js
@@ -1,3 +1,17 @@
+/**
+ * This file contains Immediately Invoked Function Expressions (IIFEs)
+ * 
+ * Two self-executing functions that demonstrate database connection logging:
+ * - chai(): Logs first database connection message
+ * - aurcode(): Logs second database connection message
+ * 
+ * IIFEs are used here to create private scopes and execute immediately
+ * The functions are wrapped in parentheses to treat them as expressions
+ * Each function is immediately called using the trailing ()
+ * 
+ * @file life.js
+ * @module scope
+ */
 (function chai(){
     console.log(`Db connected`);
     
