0930e07473ef2ddf8e49d6f422f5706bab2d5edb
Hari06om <hariohmverma96@gmail.com>
2025-11-03 17:14:07 +0530

    just learn if else

--- control/one.js
+++ control/one.js
@@ -1,10 +1,32 @@
-// if  
+// This code checks if a user is logged in and logs a message to the console if true.
 
-const isuserLoggedin = true
+// Declare a constant variable to indicate if the user is logged in
+// const isuserLoggedin = true;
 
-if(isuserLoggedin){
-    console.log("user logg in ");
-       
-}   
+// // Check if the user is logged in
+// if (isuserLoggedin) {
+//     // If the condition is true, log the message to the console
+//     console.log("user logg in ");
+// }
+
+// The following are comparison operators in JavaScript:
+// <  : Less than
+// >  : Greater than
+// <= : Less than or equal to
+// >= : Greater than or equal to
+// == : Equal to (value only)
+// != : Not equal to (value only)
+// ===: Strict equal to (value and type)
+// !==: Strict not equal to (value and type)
 
 
+const temp =50
+
+if(temp === 50){
+    console.log("Less then 50 ");
+    
+}else{
+    console.log("Temperature is greater than 50 ");
+}
+
+console.log("Excute");
