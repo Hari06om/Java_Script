d697cbe49efd00e0cd4120632fcf7f222550ec3d
Hari06om <hariohmverma96@gmail.com>
2025-11-04 19:02:17 +0530

    table in js

--- iteration/one.js
+++ iteration/one.js
@@ -20,7 +20,8 @@ for (let i = 0; i <=10; i++) {
     console.log(`outer loop value:${i}`);
     
     for (let j = 0; j<=10; j++) {
-        console.log(`inner loop value:${j}and outer loop is ${i}`);
+        // console.log(`inner loop value:${j}and outer loop is ${i}`);
+     console.log(i+" * " + j + " = " +i*j);
         
     }
 }
