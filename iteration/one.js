53e1995f539ea161439711a42dfffd2affa4ccbc
Hari06om <hariohmverma96@gmail.com>
2025-11-04 19:00:48 +0530

    loop  in loop

--- iteration/one.js
+++ iteration/one.js
@@ -1,10 +1,26 @@
 // for 
 
-for (let index = 0; index <= 10; index++) {
-    const element = index;
-    console.log(element);
+// for (let index = 0; index <= 10; index++) {
+//     /**
+//      * The current element from the array being iterated
+//      * @type {*} The value at the current index position
+//      */
+//     const element = index;
+//         if(element){
+//             console.log("5 is the best element ");
+//         }
+//     console.log(element);
+        
+
+// }
 
 
+
+for (let i = 0; i <=10; i++) {
+    console.log(`outer loop value:${i}`);
+    
+    for (let j = 0; j<=10; j++) {
+        console.log(`inner loop value:${j}and outer loop is ${i}`);
+        
+    }
 }
-
-
