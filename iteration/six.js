083e0d9f62c7a5b638eaf272ddc15680ea22aa0a
Hari06om <hariohmverma96@gmail.com>
2025-11-05 12:16:39 +0530

    filter in js

---
+++ iteration/six.js
@@ -0,0 +1,24 @@
+// const coding = ["js","html","css","Java","Cpp"]
+
+// const value = coding.forEach((item)=>{
+//     // console.log(item);
+//     return item ;
+// })
+
+// console.log(value);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// // Using filter method - more concise way
+// const filteredNums = myNums.filter(num => num > 4)
+// console.log('Using filter:', filteredNums)
+
+// // Using forEach method - alternative way
+// const newNums = []
+// myNums.forEach(num => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+// console.log('Using forEach:', newNums)
+
