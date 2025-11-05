57f647278b7a2f451d9efe114dc2087e4902874d
Hari06om <hariohmverma96@gmail.com>
2025-11-05 13:18:42 +0530

    shopping cart

--- iteration/nine.js
+++ iteration/nine.js
@@ -3,13 +3,44 @@ const mynums = [1,2,3]
 
 // Use reduce method to sum up all numbers in the array
 // reduce takes a callback function and an initial value (0 in this case)
-const myTotal = mynums.reduce(function (acc, currval){
-    // Log the current state of accumulator and current value in each iteration
-    console.log(`acc: ${ acc} and currval: ${currval}`);
+// const myTotal = mynums.reduce(function (acc, currval){
+//     // Log the current state of accumulator and current value in each iteration
+//     console.log(`acc: ${ acc} and currval: ${currval}`);
     
-    // Return the sum of accumulator and current value for next iteration
-    return acc + currval
-}, 0)
+//     // Return the sum of accumulator and current value for next iteration
+//     return acc + currval
+// }, 0)
 
-// Print the final total
-console.log(myTotal);
+// const myTotal = mynums.reduce((acc,curr) => acc+curr,0)
+
+// // Print the final total
+// console.log(myTotal);
+
+
+
+const shoppingCart = [ 
+    {
+    itemName: "js course",
+    price: 2999
+    },
+    {
+    itemName: "html course",
+    price: 5999
+    },
+    {
+    itemName: "moblie dev course",
+    price: 19999
+    },
+    {
+    itemName: "data science course",
+    price: 12999
+    },
+    {
+    itemName: "dev course",
+    price: 12399
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc,item)=> acc+item.price,0)
+
+console.log(priceToPay)
